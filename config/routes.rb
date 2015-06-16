7be91ac6ffa78872cb7b589216a13dba3b1beb92
Maxime Braud <mx.braud@gmail.com>
2015-06-16 16:39:07 +0200

    task mgr working well

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,17 @@
 Rails.application.routes.draw do
-  get "tasks", to: "tasks#index"
 
-  get 'tasks/new', to: "tasks#new", as: "create"
+  resources :tasks
+  # get "tasks", to: "tasks#index"
 
-  get "tasks/:id", to: "tasks#show", as: "task"
-  post "tasks", to: "tasks#create"
+  # get 'tasks/new', to: "tasks#new"
 
-  get "tasks/:id/edit", to: "tasks#edit", as: "edit"
-  patch "tasks/:id", to: "tasks#update"
+  # get "tasks/:id", to: "tasks#show", as: "task"
+  # post "tasks", to: "tasks#create"
 
-  delete "tasks/:id", to: "tasks#destroy", as: "delete"
+  # get "tasks/:id/edit", to: "tasks#edit", as: "edit"
+  # patch "tasks/:id", to: "tasks#update"
+
+  # delete "tasks/:id", to: "tasks#destroy", as: "delete"
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
