ec462f9809413e577434c8cdf43c5f719ec3cefa
Maxime Braud <mx.braud@gmail.com>
2015-06-16 11:44:30 +0200

    create todo manager on rails

---
+++ config/routes.rb
@@ -0,0 +1,84 @@
+Rails.application.routes.draw do
+  get 'task/index'
+
+  get 'task/show'
+
+  get 'task/new'
+
+  get 'task/create'
+
+  get 'task/edit'
+
+  get 'task/update'
+
+  get 'task/destroy'
+
+  get 'task_controller/index'
+
+  get 'task_controller/show'
+
+  get 'task_controller/new'
+
+  get 'task_controller/create'
+
+  get 'task_controller/edit'
+
+  get 'task_controller/update'
+
+  get 'task_controller/destroy'
+
+  # The priority is based upon order of creation: first created -> highest priority.
+  # See how all your routes lay out with "rake routes".
+
+  # You can have the root of your site routed with "root"
+  # root 'welcome#index'
+
+  # Example of regular route:
+  #   get 'products/:id' => 'catalog#view'
+
+  # Example of named route that can be invoked with purchase_url(id: product.id)
+  #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
+
+  # Example resource route (maps HTTP verbs to controller actions automatically):
+  #   resources :products
+
+  # Example resource route with options:
+  #   resources :products do
+  #     member do
+  #       get 'short'
+  #       post 'toggle'
+  #     end
+  #
+  #     collection do
+  #       get 'sold'
+  #     end
+  #   end
+
+  # Example resource route with sub-resources:
+  #   resources :products do
+  #     resources :comments, :sales
+  #     resource :seller
+  #   end
+
+  # Example resource route with more complex sub-resources:
+  #   resources :products do
+  #     resources :comments
+  #     resources :sales do
+  #       get 'recent', on: :collection
+  #     end
+  #   end
+
+  # Example resource route with concerns:
+  #   concern :toggleable do
+  #     post 'toggle'
+  #   end
+  #   resources :posts, concerns: :toggleable
+  #   resources :photos, concerns: :toggleable
+
+  # Example resource route within a namespace:
+  #   namespace :admin do
+  #     # Directs /admin/products/* to Admin::ProductsController
+  #     # (app/controllers/admin/products_controller.rb)
+  #     resources :products
+  #   end
+end
